fa694ccfb39491ee1b5b4abbff4a7c43397d3637
maria-kaki <maria.oliveira170922@gmail.com>
2024-05-08 22:15:28 -0300

    aula85

---
+++ aula0805/exe1/script.js
@@ -0,0 +1,14 @@
+
+
+function processar(event,escolha){
+
+}
+
+const pessoas = ["Caio","Maria Clara","Fulano",15,true
+// {
+//     nome: 'Maria Clara',
+//     cpf: 'hcsdkjcsd',
+//     endereco: 'kfjoasrdjnfhkivanbl'
+// }
+]
+
