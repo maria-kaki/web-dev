cacad64607ec7211cfdb9be3bdc232e78238c4a1
maria-kaki <maria.oliveira170922@gmail.com>
2024-05-08 22:46:07 -0300

    certo

--- aula0805/exe1/script.js
+++ aula0805/exe1/script.js
@@ -1,14 +1,37 @@
 
 
-function processar(event,escolha){
-
+function computador(){
+    const jokeipo = ["pedra","papel","tesoura"]
+    let numeroaleatorio = Math.floor(Math.random()*jokeipo.length)
+    return jokeipo[numeroaleatorio]
 }
 
-const pessoas = ["Caio","Maria Clara","Fulano",15,true
-// {
+function processar(escolha){
+    let escolhaPC = computador()
+    let resultado
+
+    if(escolhaPC === escolha){
+        resultado = "Empate!"
+    }
+    else if (
+        (escolha === 'papel' && escolhaPC === 'pedra') ||
+        (escolha === 'pedra' && escolhaPC === 'tesoura') ||
+        (escolha === 'tesoura' && escolhaPC === 'papel')
+    ){
+        resultado = "Ganhou!"
+    }
+    else{
+        resultado = "Perdeu!"
+    }
+
+    document.getElementById('resultado').innerText = resultado
+}
+
+// const pessoas = ["Caio","Maria Clara","Fulano",15,true
+//{
 //     nome: 'Maria Clara',
 //     cpf: 'hcsdkjcsd',
 //     endereco: 'kfjoasrdjnfhkivanbl'
 // }
-]
+//]
 
