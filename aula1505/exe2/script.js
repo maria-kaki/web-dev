9fb946b0cc6a75306a284c418d3b38be71cd7ba8
maria-kaki <maria.oliveira170922@gmail.com>
2024-05-15 22:33:39 -0300

    a

---
+++ aula1505/exe2/script.js
@@ -0,0 +1,3 @@
+function dobrar(numero){
+    return numero.map(resul => resul*2)
+}console.log(dobrar([5,8,9,10,65]))
---
+++ aula1505/exe3/script.js
@@ -0,0 +1,3 @@
+function grito(frase){
+    return frase.map(x => x.toUpperCase())
+}console.log(grito(['oiiiiiii','mariaaa']))
