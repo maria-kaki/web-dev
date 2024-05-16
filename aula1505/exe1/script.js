24fe46de83b8013397c25f54dc739b07b897bb82
maria-kaki <maria.oliveira170922@gmail.com>
2024-05-15 22:12:54 -0300

    a

--- aula1505/exe1/script.js
+++ aula1505/exe1/script.js
@@ -1,5 +1,36 @@
 const pessoas = ["Fulano", "Ciclano", "Beltrano"]
 
-pessoas.push("Maria")
+// pessoas.push("Maria") // adiciona o último
 
-console.log(pessoas)
+// pessoas.pop() // remove o último
+
+// pessoas.unshift("Roberto") // adiciona no começo
+
+// pessoas.shift() // remove no começo
+
+// pessoas[1] = "Cleiton"
+
+// console.log(pessoas)
+
+// for (i=0;i<=10;i++){
+//     console.log(pessoas[i])
+// }
+
+// for que passa por todo o Array
+// pessoas.forEach(
+//     function rodarArray(usuario){
+//         console.log(usuario.toLowerCase())
+//     }
+// )
+
+// arrow function, jeito mais curto de escrever uma função
+pessoas.forEach(usuario => console.log(usuario.toLowerCase()))
+
+// o foreach n te devolve nada (sem retorno), já o map mapeia => copia, mexe e retorna a cópia para você
+const novopessoas = pessoas.map(usuario => usuario.toLowerCase())
+console.log(novopessoas)
+// outro jeito de escrever função
+const rodarArray = usuario => {
+
+}
+rodarArray()
