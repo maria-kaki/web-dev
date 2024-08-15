35f6f8de638b32dc55b9d8db14ca6b12e3a4c9b4
maria-kaki <maria.oliveira170922@gmail.com>
2024-08-14 22:30:25 -0300

    a

---
+++ primeiro-site-react/src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <>
+            <h1>Título da página</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </> 
+     );
+}
+
+export default Header;
