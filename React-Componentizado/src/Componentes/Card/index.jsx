8ac1b9e613d86fb3f4638d7c03eab7c92ffacf5e
maria-kaki <maria.oliveira170922@gmail.com>
2024-08-28 22:25:41 -0300

    a

--- React-Componentizado/src/Componentes/Card/index.jsx
+++ React-Componentizado/src/Componentes/Card/index.jsx
@@ -1,9 +1,10 @@
-export default function Card(props){
+export default function Card({titulo,nivel,tempoxp}){
     return(
         <>
             <div className="skill-card">
-                    <h3>{props.titulo}</h3>
-                    <p>Nível: {props.nivel}</p>
+                    <h3>{titulo}</h3>
+                    <p>Nível: {nivel}</p>
+                    <p>Tempo XP: {tempoxp}</p>
             </div>
         </>
     )
--- React-Componentizado/src/Componentes/Skills/index.jsx
+++ React-Componentizado/src/Componentes/Skills/index.jsx
@@ -4,17 +4,18 @@ function Skills({ skills }) {
   return (
     <div className="skills">
       <h2>Habilidades</h2>
-                <Card titulo = "React" nivel="Lutando"/>
-                <Card titulo = "CSS Grid" nivel="Intermediário"/>
-                <Card titulo = "React" nivel="Lutando"/>
-                <div className="skill-card">
-                    <h3>CSS</h3>
-                    <p>Nível: Intermediário</p>
-                </div>
-                <div className="skill-card">
-                    <h3>JavaScript</h3>
-                    <p>Nível: Básico</p>
-                </div>
+                <Card 
+                  titulo = {"React"} 
+                  nivel = {"Lutando"}
+                  tempoxp = {"2 meses"}/>
+                <Card 
+                  titulo = {"CSS Grid" }
+                  nivel={"Intermediário"}
+                  tempoxp={"3 meses"}/>
+                <Card 
+                  titulo = {"React"} 
+                  nivel={"Lutando"}
+                  tempoxp={"4 meses"}/>
     </div>
   );
 }
