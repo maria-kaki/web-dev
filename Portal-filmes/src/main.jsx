48f20725a235cf0f85bfe6af8cea9959a368df09
maria-kaki <maria.oliveira170922@gmail.com>
2024-09-11 22:38:02 -0300

    aula

--- Portal-filmes/src/main.jsx
+++ Portal-filmes/src/main.jsx
@@ -2,18 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true,element: <Home/>},
+      {path:'/filmes', element: <MovieListPage/>},
+      {path:'/filmes/detalhes-filme', element: <MovieDetailPage/>},
+      {path:'/generos', element: <GenreListPage/>},
+      {path:'/generos/lista-por-genero', element: <MoviesByGenrePage/>},
+      {path:'*', element: <PageNotFound/>}
+    ]
+  }
+]
+  
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
